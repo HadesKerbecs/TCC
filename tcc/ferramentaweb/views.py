992b5d0c02ec7e0f95e2c47730327d01980a8643
Eduardo <eduardotm22@gmail.com>
2024-10-15 16:53:38 -0300

    Commit47

--- tcc/ferramentaweb/views.py
+++ tcc/ferramentaweb/views.py
@@ -7,82 +7,58 @@ from django.utils import timezone
 openai.api_key = ''
 
 def index(request):
+    if 'user_id' not in request.session:
+        request.session['user_id'] = str(uuid.uuid4())
+    user_id = request.session['user_id']
+    historico_salvo = Historico_Conversa.objects.filter(user_id=user_id).order_by('timestamp')
+    
     if 'chat_historico' not in request.session:
         request.session['chat_historico'] = [
             {"role": "system", "content": "Você é um especialista em psicopatologia. Responda de forma útil e apropriada com base no contexto de psicopatologia."}
         ]
-
-    if 'user_id' not in request.session:
-        request.session['user_id'] = str(uuid.uuid4())
+    
+    for entry in historico_salvo:
+        if {"role": "user", "content": entry.message} not in request.session['chat_historico']:
+            request.session['chat_historico'].append({"role": "user", "content": entry.message})
+            request.session['chat_historico'].append({"role": "assistant", "content": entry.response})
+    
+    request.session.modified = True 
 
     context = {
-        'user_id': request.session['user_id'],
+        'user_id': user_id,
     }
-
     return render(request, 'ferramentaweb/index.html', context)
-
-def normalizar_texto(texto):
-    """Normaliza o texto removendo caracteres especiais e convertendo para minúsculas."""
-    return re.sub(r'\W+', ' ', texto.lower()).strip()
-
-def deve_continuar_conversa(user_input, chat_historico):
-    user_input_normalizado = normalizar_texto(user_input)
-
-    # Verifica se a última mensagem do assistente contém informações relacionadas
-    ultima_resposta_assistente = next((msg for msg in reversed(chat_historico) if msg['role'] == 'assistant'), None)
-    
-    if ultima_resposta_assistente:
-        ultima_resposta = normalizar_texto(ultima_resposta_assistente['content'])
-
-        # Se a entrada do usuário mencionar alguma parte da última resposta do assistente, considera como continuidade
-        if any(term in user_input_normalizado for term in ultima_resposta.split()):
-            return True
-
-    return False
 
 def gerar_caso_stream(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
             user_input = data.get('user_input', '').strip()
-
-            # Inicializa ou recupera o histórico de conversas da sessão
+            
+            if 'chat_historico' not in request.session:
+                request.session['chat_historico'] = [
+                    {"role": "system", "content": "Você é um especialista em psicopatologia. Responda de forma útil e apropriada com base no contexto de psicopatologia."}
+                ]
+            
             chat_historico = request.session.get('chat_historico', [])
-            print("Histórico de conversa antes da nova entrada:", chat_historico)  # Log do histórico
-
-            # Verifica se o usuário deseja continuar a conversa
-            if deve_continuar_conversa(user_input, chat_historico):
-                ultima_mensagem_assistente = next((msg for msg in reversed(chat_historico) if msg['role'] == 'assistant'), None)
-                if ultima_mensagem_assistente:
-                    print("Última mensagem do assistente:", ultima_mensagem_assistente['content'])  # Log da última mensagem
-                    user_input = f"{user_input} Continuando com: {ultima_mensagem_assistente['content']}"
-
-            print("Entrada do usuário para processamento:", user_input)  # Log da entrada do usuário
-
-            # Atualiza o histórico de mensagens com a entrada do usuário
-            chat_historico.append({"role": "user", "content": user_input})
-
-            # Limite de mensagens no histórico
-            if len(chat_historico) > 50:
-                # Remove as 10 mensagens mais antigas
-                chat_historico = chat_historico[-50:]
-                print("Histórico ajustado para manter apenas as últimas 50 mensagens.")  # Log após ajuste
-
-            # Configurações de personalização e complexidade
             personalizacao = request.session.get('personalizacao', {})
             nivel_complexidade = personalizacao.get('nivel_complexidade', None)
-
+            
             if nivel_complexidade and personalizacao:
-                # Adiciona informações de personalização ao histórico
                 chat_historico.append({
-                    "role": "system", 
-                    "content": (f"O usuário tem {personalizacao['idade']} anos, sexo {personalizacao['sexo']}," 
-                                f" histórico médico: {personalizacao['historico_medico']}," 
-                                f" contexto social: {personalizacao['contexto_social']}." 
+                    "role": "system",
+                    "content": (f"O usuário tem {personalizacao['idade']} anos, sexo {personalizacao['sexo']},"
+                                f" histórico médico: {personalizacao['historico_medico']},"
+                                f" contexto social: {personalizacao['contexto_social']}."
                                 f" O nível de complexidade selecionado é {nivel_complexidade}.")
                 })
-
-            # Responde ao usuário
+            
+            chat_historico.append({"role": "user", "content": user_input})
+            
+            if len(chat_historico) > 50:
+                chat_historico = chat_historico[-50:]
+                print("Histórico ajustado para manter apenas as últimas 50 mensagens.")
+            
             def stream_response():
                 settings = {
                     'Básico': (0.5, 500),
@@ -90,13 +66,11 @@ def gerar_caso_stream(request):
                     'Avançado': (0.9, 1000)
                 }
                 temperature, max_tokens = settings.get(nivel_complexidade, (0.7, 750))
-
                 print("Configurando a chamada para a API:", {
                     "temperature": temperature,
                     "max_tokens": max_tokens,
                     "historico_mensagens": chat_historico
-                })  # Log das configurações da API
-
+                })
                 response = openai.ChatCompletion.create(
                     model="gpt-3.5-turbo",
                     messages=chat_historico,
@@ -104,7 +78,6 @@ def gerar_caso_stream(request):
                     temperature=temperature,
                     stream=True
                 )
-
                 accumulated_text = ''
                 for chunk in response:
                     if 'choices' in chunk:
@@ -114,13 +87,11 @@ def gerar_caso_stream(request):
                             if content:
                                 accumulated_text += content
                                 yield json.dumps({"response": accumulated_text}) + "\n"
-
-                # Atualiza o histórico após a resposta ser gerada
+                
                 chat_historico.append({"role": "assistant", "content": accumulated_text})
                 request.session['chat_historico'] = chat_historico
                 request.session.modified = True
-
-                # Salva no banco de dados
+                
                 Historico_Conversa.objects.create(
                     user_id=request.session['user_id'],
                     message=user_input,
@@ -128,18 +99,14 @@ def gerar_caso_stream(request):
                     timestamp=timezone.now(),
                     nivel_complexidade=nivel_complexidade
                 )
-
-                # Log da última mensagem salva
-                print("Última mensagem salva:")
                 print(f"Usuário: {user_input}")
                 print(f"Assistente: {accumulated_text}")
-
                 yield json.dumps({"response": accumulated_text}) + "\n"
-
+            
             return StreamingHttpResponse(stream_response(), content_type='application/json')
-
+        
         except Exception as e:
-            print(f"Erro durante o processamento: {str(e)}")  # Log de erro
+            print(f"Erro durante o processamento: {str(e)}")
             return StreamingHttpResponse(
                 json.dumps({'error': str(e)}),
                 content_type='application/json'
